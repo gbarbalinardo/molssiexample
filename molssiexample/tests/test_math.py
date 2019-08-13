07917538b16548040befe03572984715f06e45d4
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-08-13 11:46:53 -0400

    Add more tests for math

--- molssiexample/tests/test_math.py
+++ molssiexample/tests/test_math.py
@@ -2,6 +2,6 @@ import molssiexample as me
 import pytest
 
 
-@pytest.mark.parametrize('n, answer', [(0, 1), (1, 2), (2, 2.5)])
+@pytest.mark.parametrize('n, answer', [(0, 1), (1, 2), (2, 2.5), (3, 2.666)])
 def test_euler(n, answer):
-    assert me.math.euler(n) == answer
+    assert me.math.euler(n) == pytest.approx(answer, abs=3)
